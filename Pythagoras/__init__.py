ebe4e20550ab8f762ee8148a7ebf3c42507a199d
Vlad Pavlov <vlpavlov@ieee.org>
2020-07-07 20:15:57 -0700

    BLD:: Make feature_engineering classes accessible via package interface

--- Pythagoras/__init__.py
+++ Pythagoras/__init__.py
@@ -1,6 +1,7 @@
 from Pythagoras.util import *
 from Pythagoras.logging import *
 from Pythagoras.caching import *
+from Pythagoras.feature_engineering import *
 
 Pythagoras_logger = LoggableObject()
 Pythagoras_logger.update_parent_logger()
