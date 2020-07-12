0079ee984ed4e10ae5f7e753d2b7f936f016ec35
Vlad Pavlov <vlpavlov@ieee.org>
2020-07-11 20:14:40 -0700

    ENH: Add a transformer that removes numeric NaN-ss

--- Pythagoras/feature_engineering.py
+++ Pythagoras/feature_engineering.py
@@ -1,6 +1,9 @@
 import pandas as pd
 from copy import deepcopy
 from typing import Optional, Set, List
+
+from numpy import mean, median
+
 from Pythagoras.util import *
 from Pythagoras.logging import *
 
@@ -210,3 +213,81 @@ class Deduper(AbstractFeatureMaker):
         self.info(log_message)
 
         return X_new
+
+
+class NumericNaNsFiller(AbstractFeatureMaker):
+    """A transformer that creates NaN-less versions of numeric columns"""
+
+    nan_aggr_funcs: Optional[List[Any]]
+    nan_fill_values: Optional[pd.core.frame.DataFrame]
+
+    def __init__(self
+                 , aggr_funcs = [np.min, np.max, np.mean,percentile50 , minmode, maxmode]
+                 , *args
+                 , **kwargs
+                 ) -> None:
+        super().__init__(*args, **kwargs)
+        self.nan_fill_values = None
+        self.nan_aggr_funcs = aggr_funcs
+
+
+    @property
+    def is_fitted(self) -> bool:
+        return self.nan_fill_values is not None
+
+    def fit_transform(self
+                      , X: pd.core.frame.DataFrame
+                      , y=None
+                      ) -> pd.core.frame.DataFrame:
+
+        X_num = X.select_dtypes(include="number")
+        num_nans = int(X_num.isna().sum().sum())
+        aggr_func_names = [f.__name__ for f in self.nan_aggr_funcs]
+        n_func = len(aggr_func_names)
+
+        log_message = f"==> Starting removing NaNs from "
+        log_message += f"{len(X_num.columns)} numeric columns of a dataframe "
+        log_message += "named < " + NeatStr.object_names(X, div_ch=" / ")
+        log_message += f" > with shape {X.shape}. "
+        log_message += f"Currently, the numeric columns of a dataset"
+        log_message += f" contain {num_nans} NaN-s. "
+        log_message += f"Each numeric columns will be replaced with "
+        log_message += f"{n_func} new ones, with imputation performed "
+        log_message += f"using the following functions: {aggr_func_names}."
+        self.info(log_message)
+
+        aggregations = {}
+        for col in X_num:
+            aggregations[col] = [f(X_num[col]) for f in self.nan_aggr_funcs]
+
+        self.nan_fill_values = pd.DataFrame(
+            data=aggregations, index=aggr_func_names)
+        print()
+        return self.transform(X_num)
+
+    def transform(self
+                  , X: pd.core.frame.DataFrame
+                  ) -> pd.core.frame.DataFrame:
+
+        assert self.is_fitted
+        X_num = X.select_dtypes(include="number")
+        assert set(X_num.columns) == set(self.nan_fill_values.columns)
+        num_nans = X_num.isna().sum().sum()
+
+        all_columns = []
+        for col in X_num.columns:
+            for f in self.nan_aggr_funcs:
+                label = f.__name__
+                f_val = self.nan_fill_values.at[label, col]
+                filled_column = X[col].fillna(value=f_val)
+                filled_column.name = "fillna_" + label + "(" + col + ")"
+                all_columns += [filled_column]
+
+        result = pd.concat(all_columns, axis=1)
+        log_message = f"<== Returning a new, numeric-only dataframe"
+        log_message += f" with shape {result.shape}."
+        log_message += f" {num_nans} original NaN-s were removed"
+        log_message += f" by applying {len(self.nan_aggr_funcs)}"
+        log_message += f" imputation functions."
+        self.info(log_message)
+        return result
