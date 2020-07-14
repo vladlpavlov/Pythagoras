0e06d3e78baef307c95ab62943c93ccd2f24ec03
Vlad Pavlov <vlpavlov@ieee.org>
2020-07-13 21:50:23 -0700

    ENH: Create a base class for loggable predictors

--- Pythagoras/feature_engineering.py
+++ Pythagoras/feature_engineering.py
@@ -13,7 +13,7 @@ class AbstractFeatureMaker(LoggableObject):
     pass
 
 class AbstractFeatureMaker(LoggableObject):
-    """A base class for transformers that create new features"""
+    """A base class for loggable transformers that create new features"""
 
     def __init__(self,  *args, **kwargs) -> None:
         kwargs["reveal_calling_method"] = kwargs.get(
@@ -55,6 +55,57 @@ class AbstractFeatureMaker(LoggableObject):
             ,X:pd.DataFrame
             ) -> pd.DataFrame:
         raise NotImplementedError
+
+    @property
+    def is_fitted(self) -> bool:
+        raise NotImplementedError
+
+# Workaround to ensure compatibility with Python <= 3.6
+# Versions 3.6 and below do not support postponed evaluation
+class AbstractPredictor(LoggableObject):
+    pass
+
+class AbstractPredictor(LoggableObject):
+    """A base class for loggable predictors """
+
+    def __init__(self, *args, **kwargs) -> None:
+        kwargs["reveal_calling_method"] = kwargs.get(
+            "reveal_calling_method", True)
+        super().__init__(*args, **kwargs)
+
+    def fit(self
+            , X: pd.DataFrame
+            , y: pd.Series
+            ) -> AbstractPredictor:
+
+        if type(self) == AbstractPredictor:
+            raise NotImplementedError
+
+        X.sort_index(inplace=True)
+        y.sort_index(inplace=True)
+        assert (X.index == y.index).all()
+
+        log_message = f"==> Starting fittig a model "
+        log_message += "using a DataFrame named < "
+        log_message += NeatStr.object_names(X, div_ch=" / ")
+        log_message += f" > with the shape {X.shape}, and a Series named < "
+        log_message += NeatStr.object_names(y, div_ch=" / ") + " >."
+        self.info(log_message)
+
+    def predict(self
+                , X: pd.DataFrame
+                ) -> pd.Series:
+
+        if type(self) == AbstractPredictor:
+            raise NotImplementedError
+
+        assert self.is_fitted
+
+        log_message = f"==> Starting generating predictions "
+        log_message += "using a DataFrame named < "
+        log_message += NeatStr.object_names(X, div_ch=" / ")
+        log_message += f" > with the shape {X.shape}."
+        self.info(log_message)
 
     @property
     def is_fitted(self) -> bool:
