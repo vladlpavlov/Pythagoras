0cf97b6acd01179df03939e95560d2954c0a7247
Vlad Pavlov <vlpavlov@ieee.org>
2020-07-13 21:54:28 -0700

    BLD:: Make regression classes accessible via package interface

--- Pythagoras/__init__.py
+++ Pythagoras/__init__.py
@@ -2,6 +2,8 @@ from Pythagoras.util import *
 from Pythagoras.logging import *
 from Pythagoras.caching import *
 from Pythagoras.feature_engineering import *
+from Pythagoras.regression import *
+
 
 Pythagoras_logger = LoggableObject()
 Pythagoras_logger.update_parent_logger()
