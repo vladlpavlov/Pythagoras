1dccb3a5897d11e1ca4ff4c48d849a2e06a85f53
Vlad Pavlov <vlpavlov@ieee.org>
2020-07-18 16:54:22 -0700

    CLN: Move PRegressor and CV_Score classes to regression.py

--- Pythagoras/feature_engineering.py
+++ Pythagoras/feature_engineering.py
@@ -146,73 +146,6 @@ class PEstimator(LoggableObject):
         raise NotImplementedError
 
 
-class PRegressor(PEstimator):
-    target_name_: Optional[str]
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def start_fitting(self
-                      , X: Any
-                      , y: Any
-                      , write_to_log: bool = True
-                      ) -> Tuple[pd.DataFrame, pd.Series]:
-        X, y = super().start_fitting(X, y)
-        self.target_name_ = y.name
-        self.min_med_max_ = (min(y), percentile50(y), max(y))
-        return X, y
-
-    def start_predicting(self
-                         , X: pd.DataFrame
-                         , write_to_log: bool = True
-                         ) -> pd.DataFrame:
-
-        assert self.is_fitted_
-        assert len(X)
-
-        if write_to_log:
-            log_message = f"==> Starting generating predictions "
-            log_message += f"using a {type(X).__name__} named < "
-            log_message += NeatStr.object_names(X, div_ch=" / ")
-            log_message += f" > with the shape {X.shape}."
-            self.info(log_message)
-
-        X = self._fix_X(X)
-        assert set(self.input_columns_) <= set(X)
-        X = deepcopy(X[self.input_columns_])
-
-        if not self.input_can_have_nans:
-            assert X.isna().sum().sum() == 0
-
-        return X
-
-    def finish_predicting(self
-                          , y: pd.Series
-                          , write_to_log: bool = True
-                          ) -> pd.DataFrame:
-
-        assert len(y)
-
-        n_val = len(y)
-        p_min_med_max = (min(y), percentile50(y), max(y))
-        n_nans = y.isna().sum()
-
-        if write_to_log:
-            log_message = f"<== Predictions for {y.name} have been created. "
-            log_message += f"The result contains {n_val} values "
-            log_message += f"with {n_nans} NaN-s, with the following "
-            log_message += f"min, median, max: {p_min_med_max}; "
-            log_message += f"while the taining data had {self.min_med_max_}."
-            self.info(log_message)
-
-        if not self.output_can_have_nans:
-            assert n_nans == 0
-
-        y.name = self.target_name_
-
-        return y
-
-
 class PFeatureMaker(PEstimator):
 
     def __init__(self, *args, **kwargs):
@@ -593,19 +526,6 @@ class NumericImputer(PFeatureMaker):
         self.info(log_message)
 
         return self.finish_transforming(result, write_to_log=False)
-
-
-class CV_Score(LoggableObject):
-    def __init__(self, model, n_splits=3, n_repeats=3):
-        self.rkf = RepeatedKFold(n_splits=n_splits, n_repeats=n_repeats)
-        self.model = clone(model)
-
-    def __call__(self, X, y, ):
-        self.scores_ = cross_val_score(self.model, X, y, cv=self.rkf,
-                                       scoring="r2")
-        mean_score = np.mean(self.scores_)
-        median_score = np.median(self.scores_)
-        return min(mean_score, median_score)
 
 
 class NumericFuncTransformer(PFeatureMaker):
--- Pythagoras/regression.py
+++ Pythagoras/regression.py
@@ -2,55 +2,92 @@ import pandas as pd
 
 from Pythagoras.util import *
 from Pythagoras.logging import *
+from Pythagoras.feature_engineering import *
+
 
 
 # Workaround to ensure compatibility with Python <= 3.6
 # Versions 3.6 and below do not support postponed evaluation
-class AbstractPredictor(LoggableObject):
-    pass
 
-class AbstractPredictor(LoggableObject):
-    """A base class for loggable predictors """
 
-    def __init__(self, *args, **kwargs) -> None:
-        kwargs["reveal_calling_method"] = kwargs.get(
-            "reveal_calling_method", True)
+
+class CV_Score(LoggableObject):
+    def __init__(self, model, n_splits=3, n_repeats=3):
+        self.rkf = RepeatedKFold(n_splits=n_splits, n_repeats=n_repeats)
+        self.model = clone(model)
+
+    def __call__(self, X, y, ):
+        self.scores_ = cross_val_score(self.model, X, y, cv=self.rkf,
+                                       scoring="r2")
+        mean_score = np.mean(self.scores_)
+        median_score = np.median(self.scores_)
+        return min(mean_score, median_score)
+
+
+class PRegressor(PEstimator):
+    target_name_: Optional[str]
+
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def fit(self
-            , X: pd.DataFrame
-            , y: pd.Series
-            ) -> AbstractPredictor:
+    def start_fitting(self
+                      , X: Any
+                      , y: Any
+                      , write_to_log: bool = True
+                      ) -> Tuple[pd.DataFrame, pd.Series]:
+        X, y = super().start_fitting(X, y)
+        self.target_name_ = y.name
+        self.min_med_max_ = (min(y), percentile50(y), max(y))
+        return X, y
 
-        if type(self) == AbstractPredictor:
-            raise NotImplementedError
+    def start_predicting(self
+                         , X: pd.DataFrame
+                         , write_to_log: bool = True
+                         ) -> pd.DataFrame:
 
-        X.sort_index(inplace=True)
-        y.sort_index(inplace=True)
-        assert (X.index == y.index).all()
+        assert self.is_fitted_
+        assert len(X)
 
-        log_message = f"==> Starting fittig a model "
-        log_message += "using a DataFrame named < "
-        log_message += NeatStr.object_names(X, div_ch=" / ")
-        log_message += f" > with the shape {X.shape}, and a Series named < "
-        log_message += NeatStr.object_names(y, div_ch=" / ") + " >."
-        self.info(log_message)
+        if write_to_log:
+            log_message = f"==> Starting generating predictions "
+            log_message += f"using a {type(X).__name__} named < "
+            log_message += NeatStr.object_names(X, div_ch=" / ")
+            log_message += f" > with the shape {X.shape}."
+            self.info(log_message)
 
-    def predict(self
-                , X: pd.DataFrame
-                ) -> pd.Series:
+        X = self._fix_X(X)
+        assert set(self.input_columns_) <= set(X)
+        X = deepcopy(X[self.input_columns_])
 
-        if type(self) == AbstractPredictor:
-            raise NotImplementedError
+        if not self.input_can_have_nans:
+            assert X.isna().sum().sum() == 0
 
-        assert self.is_fitted
+        return X
 
-        log_message = f"==> Starting generating predictions "
-        log_message += "using a DataFrame named < "
-        log_message += NeatStr.object_names(X, div_ch=" / ")
-        log_message += f" > with the shape {X.shape}."
-        self.info(log_message)
+    def finish_predicting(self
+                          , y: pd.Series
+                          , write_to_log: bool = True
+                          ) -> pd.DataFrame:
 
-    @property
-    def is_fitted(self) -> bool:
-        raise NotImplementedError
+        assert len(y)
+
+        n_val = len(y)
+        p_min_med_max = (min(y), percentile50(y), max(y))
+        n_nans = y.isna().sum()
+
+        if write_to_log:
+            log_message = f"<== Predictions for {y.name} have been created. "
+            log_message += f"The result contains {n_val} values "
+            log_message += f"with {n_nans} NaN-s, with the following "
+            log_message += f"min, median, max: {p_min_med_max}; "
+            log_message += f"while the taining data had {self.min_med_max_}."
+            self.info(log_message)
+
+        if not self.output_can_have_nans:
+            assert n_nans == 0
+
+        y.name = self.target_name_
+
+        return y
+
+
