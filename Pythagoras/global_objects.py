749b31cad9e48f018e1d403fbb6ed47a09e846c9
Vlad Pavlov <vlpavlov@ieee.org>
2020-10-22 15:08:14 -0700

    ENH: Remove global logger and cache objects

--- Pythagoras/global_objects.py
+++ Pythagoras/global_objects.py
@@ -2,8 +2,3 @@ from Pythagoras.util import *
 from Pythagoras.logging import *
 from Pythagoras.feature_engineering import *
 from Pythagoras.caching import *
-
-# Pythagoras_logger = LoggableObject()
-# Pythagoras_logger.update_parent_logger()
-
-Pythagoras_cache = PickleCache()
