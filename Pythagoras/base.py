a80829905a3babd23bc798e4938ec0f075322db2
Vlad Pavlov <vlpavlov@ieee.org>
2020-10-25 00:06:11 -0700

    ENH: Rename NotProvited singletom to NotKnown

--- Pythagoras/base.py
+++ Pythagoras/base.py
@@ -18,12 +18,12 @@ from Pythagoras.splitters import *
 class NotKnownType:
     """ Singleton for 'NotKnown' constant """
 
-    not_provided_single_instance = None
+    not_known_single_instance = None
 
     def __new__(cls):
         if cls.not_known_single_instance is None:
             cls.not_known_single_instance = super().__new__(cls)
-        return cls.not_provided_single_instance
+        return cls.not_known_single_instance
 
 NotKnown = NotKnownType()
 
