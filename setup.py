25e5c583a9f58e19ded975bb80a8a59568842134
Vlad Pavlov <vlpavlov@ieee.org>
2020-10-25 22:07:13 -0700

    BLD: Increase version # to 0.2.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="Pythagoras"
-    ,version="0.2.5"
+    ,version="0.2.6"
     ,author="Vlad(imir) Pavlov"
     ,author_email="vlpavlov@ieee.org"
     ,description="Advanced Python tools for Data Scientists"
