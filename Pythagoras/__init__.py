dcf3d8ee0e2ba967bd4b54fa904e5403c0b34140
Vlad Pavlov <vlpavlov@ieee.org>
2022-03-10 23:29:08 -0800

    CLN: remove legacy code

--- Pythagoras/__init__.py
+++ Pythagoras/__init__.py
@@ -5,13 +5,6 @@ from Pythagoras.misc_utils import *
 from Pythagoras.not_known import *
 from Pythagoras.loggers import *
 from Pythagoras.caching import *
-from Pythagoras.splitters import *
-from Pythagoras.filters import *
-from Pythagoras.base import *
-
-
-
-from Pythagoras.global_objects import *
 
 
 
