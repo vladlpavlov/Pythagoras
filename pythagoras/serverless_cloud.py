348cb6cd7dc4d9af27b12da57ed5f45df754da80
Vlad Pavlov <vlpavlov@ieee.org>
2022-03-21 22:23:49 -0700

    CLN: improve naming for ServerlessCloud methods and helper functions

--- pythagoras/serverless_cloud.py
+++ pythagoras/serverless_cloud.py
@@ -1,11 +1,19 @@
 from random import shuffle
 
-def sync_remote(**kargs):
-    return kargs
+class ArgsDict(dict):
+    def __init__(self,*args, **kargs):
+        super().__init__(*args, **kargs)
+
+def remote_args(**kargs):
+    """ helper function to be used with .sync_parallel and similar methods"""
+    result = ArgsDict()
+    for k in sorted(kargs.keys()):
+        result[k] = kargs[k]
+    return result
 
 class ServerlessCloud:
-    def __init__(self, *args, **kargs):
-        pass
+    def __init__(self, requires:str, **kargs):
+        self.requires = requires
 
     def add(self, a_func):
         assert callable(a_func)
@@ -16,7 +24,7 @@ class ServerlessCloud:
             shuffle(ret_rez)
             return ret_rez
 
-        a_func.parallel = prll
+        a_func.sync_parallel = prll
         a_func.sync_remote = a_func
 
         return a_func
