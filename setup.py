5c1df0124c2eb24d44ac708c3772840b08caa169
Vlad Pavlov <vlpavlov@ieee.org>
2022-03-25 22:28:20 -0700

    BLD: increase version to 0.3.14

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="pythagoras"
-    ,version="0.3.13"
+    ,version="0.3.14"
     ,author="Volodymyr (Vlad) Pavlov"
     ,author_email="vlpavlov@ieee.org"
     ,description="Advanced Python tools for Data Scientists"
