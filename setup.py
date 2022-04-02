e5cc5d9ba81a46acb5053745075d0c2fb79759ad
Vlad Pavlov <vlpavlov@ieee.org>
2022-04-01 17:21:58 -0700

    BLD: increase version to 0.3.43

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="pythagoras"
-    ,version="0.3.42"
+    ,version="0.3.43"
     ,author="Volodymyr (Vlad) Pavlov"
     ,author_email="vlpavlov@ieee.org"
     ,description= "Simple and inexpensive way to create, deploy and run " 
