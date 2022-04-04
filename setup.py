5ca3dac8f43909abbffe348be904cf389fa809d4
Vlad Pavlov <vlpavlov@ieee.org>
2022-04-04 00:18:03 -0700

    BLD: improve building sequence, increase version to 0.3.48

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="pythagoras"
-    ,version="0.3.47"
+    ,version="0.3.48"
     ,author="Volodymyr (Vlad) Pavlov"
     ,author_email="vlpavlov@ieee.org"
     ,description= "Simple and inexpensive way to create, deploy and run " 
