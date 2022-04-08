c682aced0221b011ad8882a6f2838a297ac084e5
Vlad Pavlov <vlpavlov@ieee.org>
2022-04-07 17:55:34 -0700

    CLN: fix outcomes of a wrong merge with https://github.com/vladlpavlov/Pythagoras/pull/14

--- pythagoras/dicts.py
+++ pythagoras/dicts.py
@@ -1,31 +1,23 @@
 import os
-import platform
-from _socket import gethostname
-from datetime import datetime
-from getpass import getpass, getuser
-from socket import socket
-from zoneinfo import ZoneInfo
-
-import pandas as pd
-
-import string
+import pickle
 from abc import *
 from typing import Set, Any, Tuple, Union, Sequence
 import boto3
-
+from pythagoras.global_objects import *
 
 import jsonpickle
 import jsonpickle.ext.numpy as jsonpickle_numpy
 import jsonpickle.ext.pandas as jsonpickle_pandas
+
 jsonpickle_numpy.register_handlers()
 jsonpickle_pandas.register_handlers()
 
-from pythagoras.global_objects import *
 
 SimpleDictKey = Union[ str, Sequence[str] ]
 """ A value which can be used as a key for SimplePersistentDict. 
 
 SimpleDictKey must be a string or a sequence of strings.
+Thec characters within strings are restricted to allowed_key_chars set.
 """
 
 class SimplePersistentDict(ABC):
@@ -238,7 +230,8 @@ class FileDirDict(SimplePersistentDict):
 
     def _read_from_file(self, file_name: str):
         if self.file_type == "pkl":
-            result = pd.read_pickle(file_name)
+            with open(file_name, 'rb') as f:
+                result = pickle.load(f)
         elif self.file_type == "json":
             with open(file_name, 'r') as f:
                 result = jsonpickle.loads(f.read())
@@ -248,7 +241,8 @@ class FileDirDict(SimplePersistentDict):
 
     def _save_to_file(self, file_name: str, value:Any):
         if self.file_type == "pkl":
-            pd.to_pickle(value, file_name) # TODO: Remove dependency from pandas
+            with open(file_name, 'wb') as f:
+                pickle.dump(value, f)
         elif self.file_type == "json":
             with open(file_name, 'w') as f:
                 f.write(jsonpickle.dumps(value, indent=4))
