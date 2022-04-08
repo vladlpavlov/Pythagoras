f725fb639e279454fbeceba530217c98849925a9
Vlad Pavlov <vlpavlov@ieee.org>
2022-04-07 17:57:36 -0700

    DOC: fix typo in a comment

--- pythagoras/global_objects.py
+++ pythagoras/global_objects.py
@@ -1,4 +1,4 @@
 import string
 
 allowed_key_chars = set(string.ascii_letters + string.digits + "()_-.=")
-""" a set of characters allowd in Persistent Dict kes, filenames / S3 objectnames, and hash addresses"""
+""" a set of characters allowd in Persistent Dict keys, filenames / S3 objectnames, and hash addresses. """
