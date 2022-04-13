22fb7a2acf21dd0edf01af9eb992608fd0827869
Vlad Pavlov <vlpavlov@ieee.org>
2022-04-12 17:15:33 -0700

    CLN: remove placeholders for timeboxed dicts

--- pythagoras/__init__.py
+++ pythagoras/__init__.py
@@ -1,7 +1,6 @@
 from pythagoras.global_objects import *
 from pythagoras.utils import *
 from pythagoras.dicts import *
-from pythagoras.timeboxed_dicts import *
 from pythagoras.p_hash_address import *
 from pythagoras.not_known import *
 from pythagoras.loggers import *
