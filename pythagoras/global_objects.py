a2cadb2b97c311075a6c38f68f2c3c17bfd937e3
Vlad Pavlov <vlpavlov@ieee.org>
2022-04-15 17:15:49 -0700

    ENH: add ""~" to a set of allowed characters

--- pythagoras/global_objects.py
+++ pythagoras/global_objects.py
@@ -1,4 +1,7 @@
 import string
 
-allowed_key_chars = set(string.ascii_letters + string.digits + "()_-.=")
-""" a set of characters allowd in Persistent Dict keys, filenames / S3 objectnames, and hash addresses. """
+allowed_key_chars = set(string.ascii_letters + string.digits + "()_-~.=")
+""" A set of URL/filename-safe characters.
+
+ A set of characters allowed in Persistent Dict keys, 
+ filenames / S3 object names, and hash addresses. """
