9649363f898f2fb5d57023ec2a6045744e679f58
Vlad Pavlov <vlpavlov@ieee.org>
2022-04-16 20:39:10 -0700

    CLN: drop legacy code

--- pythagoras/__init__.py
+++ pythagoras/__init__.py
@@ -3,12 +3,4 @@ from pythagoras.utils import *
 from pythagoras.dicts import *
 from pythagoras.p_hash_address import *
 from pythagoras.not_known import *
-from pythagoras.loggers import *
-from pythagoras.caching import *
 from pythagoras.p_cloud import *
-
-
-
-
-
-
