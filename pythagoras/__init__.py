38226619eb10e1e64b8f8ce3ff5288bd5f8917d9
Vlad Pavlov <vlpavlov@ieee.org>
2022-04-29 16:57:44 -0700

    CLN: drop NotKnown

--- pythagoras/__init__.py
+++ pythagoras/__init__.py
@@ -2,5 +2,4 @@ from pythagoras.global_objects import *
 from pythagoras.utils import *
 from pythagoras.dicts import *
 from pythagoras.p_hash_address import *
-from pythagoras.not_known import *
 from pythagoras.p_cloud import *
