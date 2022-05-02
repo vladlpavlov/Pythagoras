188c02ec65338f43d6d35ba21b52720d7f08b6f5
Vlad Pavlov <vlpavlov@ieee.org>
2022-05-01 22:15:16 -0700

    DOC: add package docstring to __init__.py

--- pythagoras/__init__.py
+++ pythagoras/__init__.py
@@ -1,3 +1,12 @@
+"""Pythagoras aims to democratize access to distributed serverless compute.
+
+We make it simple and inexpensive to create, deploy and run
+cloud-scale massively parallel algorithms from within local
+Python scripts and notebooks. Pythagoras makes data scientists' lives easier,
+while allowing them to solve more complex problems
+in a shorter time with smaller budgets.
+"""
+
 from pythagoras.global_objects import *
 from pythagoras.utils import *
 from pythagoras.dicts import *
