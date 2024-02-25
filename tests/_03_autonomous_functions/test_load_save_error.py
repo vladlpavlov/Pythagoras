05a502d4362f1d53a83cef1cb12f8bac29f1d5c6
Vlad Pavlov <vlpavlov@ieee.org>
2024-02-25 13:16:13 -0800

    TST: Add unit-tests for autonomous functions

---
+++ tests/_03_autonomous_functions/test_load_save_error.py
@@ -0,0 +1,35 @@
+from pythagoras._05_mission_control.global_state_management import (
+    _clean_global_state, initialize)
+from pythagoras._01_foundational_objects.value_addresses import ValueAddress
+from pythagoras._03_autonomous_functions import *
+
+import pytest
+
+import pythagoras as pth
+
+
+def test_load_save_error(tmpdir):
+    _clean_global_state()
+    initialize(base_dir=tmpdir, default_island_name="test", cloud_type="local")
+    assert len(pth.value_store) == 0
+    def f(a, b):
+        return a + b
+
+    f_1 = AutonomousFunction(f, island_name="test")
+    f_1_address = ValueAddress(f_1)
+    assert len(pth.value_store) == 1
+    del pth.all_autonomous_functions["test"]
+
+    def f(a, b):
+        return a * b * 2
+
+    f_2 = AutonomousFunction(f, island_name="test")
+    f_2_address = ValueAddress(f_2)
+    assert len(pth.value_store) == 2
+    del pth.all_autonomous_functions["test"]
+
+    f_a = f_1_address.get()
+    assert f_a(a=1, b=2) == 3
+
+    with pytest.raises(Exception):
+        f_b = f_2_address.get()
---
+++ tests/_03_autonomous_functions/test_save_load.py
@@ -0,0 +1,31 @@
+from pythagoras._05_mission_control.global_state_management import (
+    _clean_global_state, initialize)
+from pythagoras._01_foundational_objects.value_addresses import ValueAddress
+from pythagoras._03_autonomous_functions import *
+
+import pytest
+
+import pythagoras as pth
+
+def test_load_save(tmpdir):
+    _clean_global_state()
+    initialize(base_dir=tmpdir, default_island_name="test", cloud_type="local")
+
+    def f(a, b):
+        return a + b
+
+    f_1 = AutonomousFunction(f, island_name="test")
+    f_address = ValueAddress(f_1)
+
+    f_2 = f_address.get()
+    assert f_2(a=1, b=2) == f(a=1, b=2) == 3
+    assert f_2.name == f_1.name
+    assert f_2.naked_source_code == f_1.naked_source_code
+    assert f_2.island_name == f_1.island_name
+
+    del pth.all_autonomous_functions["test"]
+
+    f_3 = f_address.get()
+    assert f_3(a=1, b=2) == f(a=1, b=2) == 3
+    assert f_3.name == "f"
+    assert f_3.island_name == "test"
