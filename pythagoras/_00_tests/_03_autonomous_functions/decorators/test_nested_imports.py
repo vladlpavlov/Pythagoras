26bf59d6c3ed10409c228d7503ff387f0f33b8ba
Vlad Pavlov <vlpavlov@ieee.org>
2024-02-28 13:56:46 -0800

    TST: add unit-tests for @autonomous()

---
+++ pythagoras/_00_tests/_03_autonomous_functions/decorators/test_nested_imports.py
@@ -0,0 +1,41 @@
+import pytest
+
+from pythagoras._03_autonomous_functions import autonomous
+from pythagoras._05_mission_control.global_state_management import (
+    _clean_global_state, initialize)
+
+
+def test_nested_from_import(tmpdir):
+    _clean_global_state()
+    initialize(base_dir=tmpdir)
+
+    @autonomous()
+    def f(x:float)->float:
+        from math import sqrt
+        return sqrt(x)
+
+    assert f(x=4) == 2
+
+
+def test_nested_import_as(tmpdir):
+    _clean_global_state()
+    initialize(base_dir=tmpdir)
+
+    @autonomous()
+    def f(x:float)->float:
+        import math as mm
+        return mm.sin(x)
+
+    assert f(x=0) == 0
+
+def test_nested_from_import_as(tmpdir):
+    _clean_global_state()
+    initialize(base_dir=tmpdir)
+
+    @autonomous()
+    def f(x:float)->float:
+        from math import log as l
+        return l(x)
+
+    assert f(x=1) == 0
+
