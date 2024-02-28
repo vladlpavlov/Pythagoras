1057030b3812c62061a31b67658219d0d0d7ef68
Vlad Pavlov <vlpavlov@ieee.org>
2024-02-28 14:09:49 -0800

    TST: add unit-tests for @autonomous() and @idempotent()

--- pythagoras/_00_tests/_04_idempotent_functions/decorator/test_init_checks_idp.py
+++ pythagoras/_00_tests/_04_idempotent_functions/decorator/test_init_checks_idp.py
@@ -1,20 +1,26 @@
 import pytest
 
 from pythagoras._04_idempotent_functions.idempotent_decorator import idempotent
-from pythagoras._04_idempotent_functions.idempotency_checks import is_idempotent
 from pythagoras._05_mission_control.global_state_management import (
     _clean_global_state, initialize)
 
 
 def test_init_checks(tmpdir):
     _clean_global_state()
-    def f():
+    def f_1():
+        pass
+
+    def f_2():
         pass
 
     with pytest.raises(AssertionError):
-        idempotent()(f)
+        idempotent()(f_1)
+
+    idempotent(require_pth=False)(f_2)
 
     _clean_global_state()
     initialize(base_dir=tmpdir)
 
-    idempotent()(f)
+    idempotent()(f_1)
+    idempotent()(f_2)
+
