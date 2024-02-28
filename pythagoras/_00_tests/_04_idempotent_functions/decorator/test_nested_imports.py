d74ef94d7a4b28982e9283274e83cf015c44736c
Vlad Pavlov <vlpavlov@ieee.org>
2024-02-28 13:55:35 -0800

    TST: add unit-tests for @idempotent()

---
+++ pythagoras/_00_tests/_04_idempotent_functions/decorator/test_nested_imports.py
@@ -0,0 +1,42 @@
+import pytest
+
+from pythagoras._04_idempotent_functions.idempotent_decorator import idempotent
+from pythagoras._04_idempotent_functions.idempotency_checks import is_idempotent
+from pythagoras._05_mission_control.global_state_management import (
+    _clean_global_state, initialize)
+
+
+def test_nested_from_import(tmpdir):
+    _clean_global_state()
+    initialize(base_dir=tmpdir)
+
+    @idempotent()
+    def f(x:float)->float:
+        from math import sqrt
+        return sqrt(x)
+
+    assert f(x=4) == 2
+
+
+def test_nested_import_as(tmpdir):
+    _clean_global_state()
+    initialize(base_dir=tmpdir)
+
+    @idempotent()
+    def f(x:float)->float:
+        import math as mm
+        return mm.sin(x)
+
+    assert f(x=0) == 0
+
+def test_nested_from_import_as(tmpdir):
+    _clean_global_state()
+    initialize(base_dir=tmpdir)
+
+    @idempotent()
+    def f(x:float)->float:
+        from math import log as l
+        return l(x)
+
+    assert f(x=1) == 0
+
