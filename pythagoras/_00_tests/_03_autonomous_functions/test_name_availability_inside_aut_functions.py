99edfd04cff0a63df3fd91619054049042c820c2
Vlad Pavlov <vlpavlov@ieee.org>
2024-03-07 23:03:25 -0800

    TST: add unit tests

---
+++ pythagoras/_00_tests/_03_autonomous_functions/test_name_availability_inside_aut_functions.py
@@ -0,0 +1,34 @@
+import pytest
+import pythagoras as pth
+
+from pythagoras._06_mission_control.global_state_management import (
+    _clean_global_state, initialize)
+
+def test_name_availability(tmpdir):
+    _clean_global_state()
+    pth.initialize(tmpdir)
+
+    @pth.autonomous()
+    def my_good_function():
+        result = bool(pth)
+        result &= bool(pth.post_event)
+        result &= bool(pth.autonomous)
+        result &= bool(post_event)
+        return result
+
+    assert my_good_function()
+
+    # with pytest.raises(NameError):
+    @pth.autonomous()
+    def my_bad_function():
+        return bool(pytest)
+
+    with pytest.raises(AssertionError):
+        my_bad_function()
+
+    @pth.autonomous()
+    def my_2nd_bad_function():
+        return bool(pythagoras)
+
+    with pytest.raises(AssertionError):
+        my_2nd_bad_function()
