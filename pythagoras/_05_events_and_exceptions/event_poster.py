9114b0b2626547f75e605b967d86d7f354c07e41
Vlad Pavlov <vlpavlov@ieee.org>
2024-03-07 23:59:25 -0800

    ENH: implement print_event()

--- pythagoras/_05_events_and_exceptions/event_poster.py
+++ pythagoras/_05_events_and_exceptions/event_poster.py
@@ -3,8 +3,8 @@ from pythagoras._01_foundational_objects.hash_and_random_signatures import (
     get_random_signature)
 from pythagoras._05_events_and_exceptions.type_retrievers import (
     retrieve_FunctionExecutionContext_class)
-from pythagoras._05_events_and_exceptions.find_in_callstack import \
-    find_local_var_in_callstack
+from pythagoras._05_events_and_exceptions.find_in_callstack import (
+    find_local_var_in_callstack)
 from pythagoras._05_events_and_exceptions.global_event_loggers import (
     register_event_globally)
 
@@ -21,26 +21,32 @@ def get_current_function_execution_context():
 
 
 class EventPosterFactory:
-    def __init__(self):
-        pass
+    def __init__(self, enable_printing: bool = False):
+        self.enable_printing = enable_printing
+
 
     def __getitem__(self, item):
-        return EventPoster(event_type = item)
+        return EventPoster(
+            event_type = item, enable_printing = self.enable_printing)
 
     def __call__(self, **kwargs):
-        return EventPoster(event_type = None)(**kwargs)
+        event_poster =  EventPoster(
+            event_type = None, enable_printing = self.enable_printing)
+        return event_poster(**kwargs)
 
 class EventPoster:
-    def __init__(self, event_type: str|None):
+    def __init__(self, event_type: str|None, enable_printing: bool = False):
         assert isinstance(event_type, (str, type(None)))
+        assert isinstance(enable_printing, bool)
         self.event_type = event_type
+        self.enable_printing = enable_printing
 
     def __call__(self,**kwargs) -> None:
         assert pth.is_correctly_initialized(), (
             "The Pythagoras package has not been correctly initialized.")
-        context = get_current_function_execution_context()
-        if context is not None:
-            context.register_event(event_type = self.event_type, **kwargs)
+        fe_context = get_current_function_execution_context()
+        if fe_context is not None:
+            fe_context.register_event(event_type = self.event_type, **kwargs)
         else:
             if self.event_type is not None:
                 kwargs["event_type"] = self.event_type
@@ -49,5 +55,12 @@ class EventPoster:
                 event_id = get_random_signature()
             register_event_globally(**kwargs, event_id = event_id)
 
-post_event = EventPosterFactory()
+        if self.enable_printing:
+            print("\n" + 15*"~" + " EVENT WAS POSTED " + 15*"~")
+            for k in kwargs:
+                print(f"\t{k}: {kwargs[k]}")
+
+
+post_event = EventPosterFactory(enable_printing = False)
+print_event = EventPosterFactory(enable_printing = True)
 
