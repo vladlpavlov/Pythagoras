6daeb1b4abb9bc8e01463c9c52c964072111bd2c
Vlad Pavlov <vlpavlov@ieee.org>
2024-03-10 15:24:35 -0700

    CLN: polish OutputSuppressor, add unit tests for it

--- pythagoras/_06_swarming/output_suppressor.py
+++ pythagoras/_06_swarming/output_suppressor.py
@@ -3,14 +3,21 @@ import sys
 
 
 class OutputSuppressor:
+    """A context manager that suppresses stdout and stderr output."""
     def __init__(self):
-        self.old_stdout = sys.stdout
-        self.old_stderr = sys.stderr
+        pass
 
     def __enter__(self):
-        sys.stdout = open(os.devnull, 'w')
-        sys.stderr = open(os.devnull, 'w')
+        """Redirect stdout and stderr to os.devnull."""
+        self.old_stdout = sys.stdout
+        self.old_stderr = sys.stderr
+        self.devnull = open(os.devnull, 'w')
+        sys.stdout = self.devnull
+        sys.stderr = self.devnull
+        return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         sys.stdout = self.old_stdout
         sys.stderr = self.old_stderr
+        if self.devnull:
+            self.devnull.close()
