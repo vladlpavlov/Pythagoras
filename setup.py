1b7013d6776882f3b18a0c4555d73e0869e3bcd8
Vlad Pavlov <vlpavlov@ieee.org>
2024-04-02 22:53:57 -0700

    BLD: elevate version to 0.10.34

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="pythagoras"
-    ,version="0.10.33"
+    ,version="0.10.34"
     ,author="Volodymyr (Vlad) Pavlov"
     ,author_email="vlpavlov@ieee.org"
     ,description= "Simple framework for planet-scale "
