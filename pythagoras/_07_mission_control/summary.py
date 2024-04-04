abdbfe3b265efa0933043aaa082a1a70f565f410
Vlad Pavlov <vlpavlov@ieee.org>
2024-04-03 17:31:43 -0700

    ENH: improve summary()

--- pythagoras/_07_mission_control/summary.py
+++ pythagoras/_07_mission_control/summary.py
@@ -65,6 +65,13 @@ def summary(include_current_session:bool = True, print_result:bool = False):
     all_params.append(runtime(
         "All available islands"
         , ", ".join(list(pth.all_autonomous_functions))))
+    for island_name, island in pth.all_autonomous_functions.items():
+        all_params.append(runtime(
+            f"# of functions in island '{island_name}'"
+            , len(island)))
+        all_params.append(runtime(
+            f"Names of functions in island '{island_name}'"
+            , ", ".join(list(island))))
 
 
     result = pd.concat(all_params)
