a3ad3a7263c24898d099e0bcd289bb9f8806f37e
Vlad Pavlov <vlpavlov@ieee.org>
2024-11-23 21:05:09 -0800

    BUG: do not create instances of BasicPortal in describe()

--- pythagoras/_100_top_level_API/top_level_API.py
+++ pythagoras/_100_top_level_API/top_level_API.py
@@ -4,9 +4,10 @@ from pythagoras import SwarmingPortal, BasicPortal
 
 
 def initialize(base_dir) -> pd.DataFrame: #TODO: refactor
+    BasicPortal._clear_all()
     portal = SwarmingPortal(base_dir=base_dir, n_background_workers=3)
     portal.__enter__()
     return portal.describe()
 
 def describe() -> pd.DataFrame: #TODO: refactor
-    return BasicPortal().portals_stack[-1].describe()
+    return BasicPortal.portals_stack[-1].describe()
