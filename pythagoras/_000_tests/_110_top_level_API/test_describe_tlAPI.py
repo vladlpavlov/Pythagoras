21b305181f031e80ae850fd4b41fa382c1b11e43
Vlad Pavlov <vlpavlov@ieee.org>
2024-11-30 01:23:46 -0800

    ENH: refactor pth.describe()

--- pythagoras/_000_tests/_110_top_level_API/test_describe_tlAPI.py
+++ pythagoras/_000_tests/_110_top_level_API/test_describe_tlAPI.py
@@ -2,10 +2,19 @@ from pythagoras import _PortalTester, SwarmingPortal
 import pythagoras as pth
 
 
-def test_describe(tmpdir): # TODO: refactor
+def test_describe_simple(tmpdir): # TODO: refactor
     with _PortalTester(SwarmingPortal
         , tmpdir, n_background_workers=0) as t:
 
         description = pth.describe()
         assert isinstance(description, pth.pd.DataFrame)
+
+
+def test_describe_two_portals(tmpdir): # TODO: refactor
+    with _PortalTester(SwarmingPortal
+        , tmpdir, n_background_workers=0) as t:
+        with SwarmingPortal(tmpdir.mkdir("dfg"), n_background_workers=0) as t2:
+
+                description = pth.describe()
+                assert isinstance(description, pth.pd.DataFrame)
 
--- pythagoras/_110_top_level_API/top_level_API.py
+++ pythagoras/_110_top_level_API/top_level_API.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from pandas import describe_option
 
 from pythagoras import SwarmingPortal, BasicPortal, DefaultLocalPortal
 
@@ -13,9 +14,18 @@ def connect_to_portal(base_dir) -> pd.DataFrame:
     portal = SwarmingPortal(base_dir=base_dir, n_background_workers=3)
     return portal.describe()
 
-def connect_to_default_local_portal() -> pd.DataFrame:
+def connect_to_default_portal() -> pd.DataFrame:
     portal = DefaultLocalPortal()
     return portal.describe()
 
-def describe() -> pd.DataFrame: #TODO: refactor
-    return BasicPortal.entered_portals_stack[-1].describe()
+def describe() -> pd.DataFrame:
+    if len(BasicPortal.all_portals) == 1:
+        return list(BasicPortal.all_portals.values())[0].describe()
+
+    all_descriptions = []
+    for i,portal in enumerate(BasicPortal.all_portals.values()):
+        description = portal.describe()
+        description.insert(0, "portal", i)
+        all_descriptions.append(description)
+    return pd.concat(all_descriptions, ignore_index=True)
+
