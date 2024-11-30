d9ad7bc2f1cbe17c98d176310f2b87ec79bbc2e5
Vlad Pavlov <vlpavlov@ieee.org>
2024-11-29 18:02:20 -0800

    BLD: elevate version to 0.15.05

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="pythagoras"
-    ,version="0.15.04"
+    ,version="0.15.05"
     ,author="Volodymyr (Vlad) Pavlov"
     ,author_email="vlpavlov@ieee.org"
     ,description= "Planet-scale distributed computing in Python."
